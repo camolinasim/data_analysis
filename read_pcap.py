e383a6a0fb3882fea068e47ecd4de547343f8b0d
Cristian Molina <cmutep@gmail.com>
2022-04-07 19:02:04 -0600

    makes csv folder and stores csvs

--- read_pcap.py
+++ read_pcap.py
@@ -9,17 +9,19 @@ import os.path
 from os import path
 import csv
 
-pcap_folder_location = "C:\\Users\\perro\Desktop\\folder_with_folders\\practicum\\data_analysis\\pcap_files"
-# Creates a CSV with parsed data as output. CSV will be created in the same folder that the selected pcap exists.
+
+
+# output: a csv named the same as the selected pcap file, just with the .csv extension.
+#  e.g. file selected = "scenario1.pcap", then, output -> "scenario1.csv"
 def open_file_explorer_and_read_pcap():
     Tk().withdraw()
 
+    path_of_selected_pcap = askopenfilename()
     # correcting path from C:/x/x/x/x.pcap -> C:\\x\\x\\x\\x\ x.pcap
-    filename_with_pcap_extension = askopenfilename()
-    filename_with_pcap_extension =os.path.normpath(filename_with_pcap_extension)
+    path_of_selected_pcap = os.path.normpath(path_of_selected_pcap)
 
-    filename = filename_with_pcap_extension.replace(".pcap", "")
-    open_this_shit = '"C:\\Program Files\\Wireshark\\tshark.exe" -r ' + filename_with_pcap_extension + ' -T fields -E header=y -E separator=, -E quote=d -E occurrence=f -e frame.number -e _ws.col.Time -e ip.src -e ip.dst -e ip.proto -e frame.len -e _ws.col.Info > me_a_moron.csv'
+    path_of_selected_pcap_no_extension = path_of_selected_pcap.replace(".pcap", "")
+    open_this_shit = '"C:\\Program Files\\Wireshark\\tshark.exe" -r ' + path_of_selected_pcap + ' -T fields -E header=y -E separator=, -E quote=d -E occurrence=f -e frame.number -e _ws.col.Time -e ip.src -e ip.dst -e ip.proto -e frame.len -e _ws.col.Info >' + path_of_selected_pcap_no_extension + '.csv'
     stream = os.popen(open_this_shit)
     output = stream.read()
     print(output)
@@ -27,13 +29,19 @@ def open_file_explorer_and_read_pcap():
 
 
 
+ #CREATING FOLDER FOR CSVs
+    pcap_folder_location = path_of_selected_pcap.rsplit('http.pcap', 1)
+    pcap_folder_location = pcap_folder_location[0]
+    print(pcap_folder_location)
 
-    # csv_folder = pcap_folder_location + "csv_files"
-    # if not path.exists(csv_folder):
-    #     os.mkdir(csv_folder)
-    # move_this_pcap = pcap_folder_location + filename + ".csv"
-    # over_here = pcap_folder_location + "csv_files\\" + filename + ".csv"
-    # shutil.move(move_this_pcap, over_here)
+    csv_folder = pcap_folder_location + "csv_files"
+    print(csv_folder)
+    if not path.exists(csv_folder):
+        os.mkdir(csv_folder)
+    csv_name = path_of_selected_pcap_no_extension + '.csv'
+    move_this_pcap = csv_name
+    over_here = csv_folder
+    shutil.move(move_this_pcap, over_here)
 
 # ---------main----------- #
 
